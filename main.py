cbdaf42f5b982fb5e3fd5ec7017879770170f43d
Takahashi0820 <goddropdd0307@gmail.com>
2020-11-26 15:26:02 +0900

    高橋の日記を追加します
    高橋の日記を追加します

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from diaries.DiarySample import DiarySample
+from diaries.TakahashiDiaryNew import TakahashiDiaryNew
 
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), TakahashiDiaryNew(), ]
 
 for d in diaries:
     print("---------------------------------")
